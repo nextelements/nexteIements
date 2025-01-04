befd76b05b1c082707b348877b1f99a4d1a6458d
Senziousjs <florianlangheinrich@gmx.de>
2025-01-04 15:43:43 +0100

    Initial Commit

--- scripts/push.sh
+++ scripts/push.sh
@@ -9,31 +9,26 @@ RESET='\033[0m'
 BOLD='\033[1m'
 UNDERLINE='\033[4m'
 
-# Standardwerte für commit_message und branch_name, falls keine übergeben wurden
 commit_message=${1:-"Initial Commit"}
 branch_name=${2:-"master"}
 
-# Überprüfen, ob ein Commit-Nachricht übergeben wurde
 if [ -z "$commit_message" ]; then
   echo "Error: Commit message is required!"
   exit 1
 fi
 
-# Überprüfen, ob ein Branch-Name übergeben wurde
 if [ -z "$branch_name" ]; then
   echo "Warning: No branch name provided. Using default 'master'."
 fi
 
-# Schritt 1: Monorepo bereinigen
-echo "[1 / 4] Cleaning monorepo..."
+echo "[1/4] Cleaning monorepo..."
 rm -rf node_modules/ package-lock.json .turbo/
 if [ $? -ne 0 ]; then
   echo "Error: Failed to clean the monorepo."
   exit 1
 fi
 
-# Schritt 2: Dateien zum Repository hinzufügen und committen
-echo "[2 / 4] Adding and committing changes..."
+echo "[2/4] Adding and committing changes..."
 git add .
 git commit -m "$commit_message"
 if [ $? -ne 0 ]; then
@@ -41,21 +36,18 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Pushen zum angegebenen Branch
-echo "[3 / 4] Pushing to branch '$branch_name'..."
+echo "[3/4] Pushing to branch '$branch_name'..."
 git push origin "$branch_name"
 if [ $? -ne 0 ]; then
   echo "Error: Git push failed."
   exit 1
 fi
 
-# Schritt 3: Abhängigkeiten installieren
-echo "[4 / 4] Installing dependencies..."
+echo "[4/4] Installing dependencies..."
 npm install
 if [ $? -ne 0 ]; then
   echo "Error: npm install failed."
   exit 1
 fi
 
-# Abschlussmeldung
-echo -e "You can now type ${GREEN}${BOLD}npm run dev${RESET}' to run the dev server."
+echo "You can now type ${GREEN}${BOLD}${UNDERLINE}npm run dev${RESET} to run the dev server."
