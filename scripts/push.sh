f009c418ea60752d223349febe32a91249fe42d9
Senziousjs <florianlangheinrich@gmx.de>
2025-01-04 15:39:51 +0100

    Initial Commit

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,12 +4,12 @@ branch_name=${2:-master}
 echo "Clean monorepo..."
 rm -rf node_modules/ package-lock.json .turbo/
 
+echo "Clone files to repository..."
 git add .
 git commit -m "$commit_message"
 git push origin "$branch_name"
 
 echo "Install dependencies..."
 npm install
-npm install --workspaces
 
 echo "You can now type \"npm run dev\" to run the dev server"
