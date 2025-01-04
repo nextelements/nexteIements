158396fea095b857be4090b4f79526feba86bbfe
Senziousjs <florianlangheinrich@gmx.de>
2025-01-04 15:39:15 +0100

    Initial Commit

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,8 +1,15 @@
+commit_message=${1:-"Initial Commit"}
+branch_name=${2:-master}
+
+echo "Clean monorepo..."
 rm -rf node_modules/ package-lock.json .turbo/
 
-git pull origin master
-git add . 
-git commit -m "Initial commit"
-git push origin master
+git add .
+git commit -m "$commit_message"
+git push origin "$branch_name"
 
+echo "Install dependencies..."
 npm install
+npm install --workspaces
+
+echo "You can now type \"npm run dev\" to run the dev server"
