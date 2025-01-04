25dc2afbf413ebd8c53dcfd019600d344ccbbd4d
Senziousjs <florianlangheinrich@gmx.de>
2025-01-04 14:37:03 +0100

    Initial commit

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,7 +2,7 @@ git pull origin master
 
 # Führe git push aus
 
-rm -rf node_modules/
+rm -rf node_modules/ package-lock.json .turbo/
 git add . 
 git commit -m "Initial commit"
 git push origin master
